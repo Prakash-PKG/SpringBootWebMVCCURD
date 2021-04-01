73ba3998675d92a835fce142003e09f30cd1c550
Prakash-PKG <prakash.garanaik18@gmail.com>
2021-04-01 17:07:48 +0530

    new variable delared

--- src/main/java/com/app/pk/controller/StudentController.java
+++ src/main/java/com/app/pk/controller/StudentController.java
@@ -28,7 +28,7 @@ public class StudentController {
 	public String showReg(Model model) {
 		int a=20;
 		int p=100;
-		int v=900;
+		int k=9999999;
 		//Form Backing Object
 		model.addAttribute("student", new Student());
 		return "StudentRegister";
